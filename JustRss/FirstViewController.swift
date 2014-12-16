d564d4d83dc3a63d48ae72ec452e0dd6f19eaac2
lastMove <lastMove@me.com>
2014-12-16 12:20:39 +0100

    Initial Commit

---
+++ JustRss/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  JustRss
+//
+//  Created by jason akakpo on 16/12/2014.
+//  Copyright (c) 2014 More Than That. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
